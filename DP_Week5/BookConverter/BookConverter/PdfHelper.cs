348b692f8d21d66d12fd7e0c1652c46b522ec794
Mrunal Patil <m.patil@student.fontys.nl>
2018-06-13 16:55:08 +0200

    merge solved

--- DP_Week5/BookConverter/BookConverter/PdfHelper.cs
+++ DP_Week5/BookConverter/BookConverter/PdfHelper.cs
@@ -1,3 +1,29 @@
+<<<<<<< HEAD
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookConverter
+{
+    class PdfHelper : IExportHelper
+    {
+        public bool Export(string path, Library library)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Mrunal Patil\Desktop\Fontys_Tue\Fontys\Semester4\BLOCK4\DPR\DPR-repo\dpr\DP_Week5\BookConverter\BookConverter\dummydata.txt");
+            Document doc = new Document(iTextSharp.text.PageSize.A4, 10, 10, 10, 10);
+            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream("dpr.pdf", FileMode.Create));
+            doc.Open();
+            Paragraph p = new Paragraph(text);
+            doc.Add(p);
+            doc.Close();
+            return true;
+        }
+=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +37,6 @@ namespace BookConverter
     class PdfAdaptee
     {
         
+>>>>>>> master
     }
 }
--- DP_Week5/BookConverter/BookConverter/XmlAdapter.cs
+++ DP_Week5/BookConverter/BookConverter/XmlAdapter.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +12,22 @@ namespace BookConverter
 {
     class XmlAdapter : IExportHelper
     {
+        XmlDocument xmldoc;
         public XmlAdapter()
         {
+<<<<<<< HEAD
+            xmldoc = new XmlDocument();
+        }
+        public bool Export(string path, Library library)
+        {
+            return false;
+=======
 
         }
         public bool Export(string path, Library library)
         {
             throw new NotImplementedException();
+>>>>>>> master
         }
     }
 }
